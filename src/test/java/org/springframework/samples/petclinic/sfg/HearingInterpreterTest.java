a357f23f02bfbf0355c5bec8efd9f9b76a8ac0e4
Radek <rdwwojcik@gmail.com>
2023-02-09 22:46:27 +0100

    #Sekcja 13 148, 151, 152, 153 - Spring jUnit5 test

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
@@ -1,15 +1,15 @@
 package org.springframework.samples.petclinic.sfg;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ActiveProfiles("base-test")
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
 public class HearingInterpreterTest {
